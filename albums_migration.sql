dd6a81051e410452cdc426780763aa88e0f44d27
Dayna Leal <dayna.r.leal@gmail.com>
2020-02-19 12:15:17 -0600

    added run-through

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,12 +6,24 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE IF NOT EXISTS albums(
                                      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                     artist_name VARCHAR(100) DEFAULT 'NONE',
-                                     record_name  VARCHAR(100) NOT NULL,
-                                     release_date INT UNSIGNED NOT NULL,
+                                     artist_name VARCHAR(80) NOT NULL,
+                                     record_name  VARCHAR(80) NOT NULL,
+                                     release_date VARCHAR(10),
                                      sales FLOAT UNSIGNED NOT NULL,
                                      genre VARCHAR(100) NOT NULL,
                                      PRIMARY KEY (id)
 );
 
 DESCRIBE albums;
+
+# add a row of data
+INSERT INTO albums( artist_name, record_name, release_date, sales, genre) values (
+                   'Eagles',
+                   'Hotel California',
+                   '1987',
+                    30030000,
+                    'rock'
+                   );
+
+SELECT * from albums;
+
